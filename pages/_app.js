b0d3a2e880da0c6b9ece7bf2119e1566be5587ae
Cheru Berhanu <cheru@cheru.dev>
2023-06-12 14:25:17 -0400

    fonts

--- pages/_app.js
+++ pages/_app.js
@@ -25,10 +25,11 @@ export default class App extends NextApp {
         />
         <NProgress color={'#ec3750'} />
         <ForceTheme theme="light" />
-          <link rel="preconnect" href="https://fonts.googleapis.com"/>
-              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-                  <link href="https://fonts.googleapis.com/css2?family=Eczar:wght@400;500;600;700;800&display=swap"
-                        rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+              <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+                  <link
+                      href="https://fonts.googleapis.com/css2?family=Eczar:wght@400;500;600;700;800&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+                      rel="stylesheet" />
           <div
             style={{
               margin: 0,
