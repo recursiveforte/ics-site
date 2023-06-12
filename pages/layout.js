b6b2dc236de297eb9fc0d4dcd5f8c4d6ab4455ac
Cheru Berhanu <cheru@cheru.dev>
2023-06-12 15:04:19 -0400

    fonts

--- pages/layout.js
+++ pages/layout.js
@@ -14,7 +14,7 @@ export default ({children}) => <>
         <Box py={[3,3]} mt={3} mb={2} mx={[2,4]} sx={{
             textAlign: "right",
         }}>
-            <Text variant={"subtitle"} sx={{            fontWeight: "bold",
+            <Text variant={"subtitle"} sx={{            fontWeight: "bold", fontFamily: `'Eczar', serif`
             }}>
             Made with ❤️ by <Link href={"https://greenmountainrobotics.org"}>Green Mountain Robotics 9101</Link> & <Link href={"https://rutlandrobotics.org"}>iBots 2370</Link>
                 <br/>Presented by FIRSTinVT, UVM CEMS, and Ben & Jerry's
